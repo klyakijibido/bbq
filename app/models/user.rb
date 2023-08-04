cf4d39f25cbb9acf65242c4eac4abe6bc4cd8bc1
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-08-04 19:31:38 +0300

    Setup user sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ApplicationRecord
   has_many :events
 
   validates :name, presence: true, length: {maximum: 35}
-  validates :email, presence: true, length: {maximum: 255}
-  validates :email, uniqueness: true
-  validates :email, format: { with: /\A[a-z0-9._%+-]+@[a-z0-9-]+.+.[a-z]{2,4}\z/ }
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "Чувак №#{rand{777}}" if self.name.blank?
+  end
 end
