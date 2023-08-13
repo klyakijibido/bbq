6e3d00e161016e68054c5d38c974e720ace92200
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-08-13 20:12:21 +0300

    Add Comment - model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
+  # юзер может создавать много событий
   has_many :events
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: 35}
 
+  # при создании нового юзера(create), перед валидацией объекта выполнить метод set-name
   before_validation :set_name, on: :create
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[show edit update]
 
-  resources :events
+  resources :events do
+    resources :comments, only: [:create, :destroy]
+  end
 end
