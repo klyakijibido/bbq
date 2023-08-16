b415119efeb71ecab04a191d588abe9ef7f878b6
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-08-16 21:10:55 +0300

    Bugfix commit: Add model subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,15 +1,15 @@
 class Subscription < ApplicationRecord
   belongs_to :event
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates :event, presence: true
 
   # проверки выполняются только если user не задан (незалогинненные)
-  validates :user_name, presence: true, unless: 'user.present?'
-  validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: 'user.present?'
+  validates :user_name, presence: true, unless: -> { user.present? }
+  validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
 
-  validates :user, uniqueness: {scope: :event_id}, if: 'user.present?'
-  validates :user_email, uniqueness: {scope: :event_id}, unless: 'user.present?'
+  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
 
   def user_name
     if user.present?
