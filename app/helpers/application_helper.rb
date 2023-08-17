8ddfdc3a39f87707d604d0031e99d33950fe8338
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-08-17 15:36:35 +0300

    Add method bi_icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     # TODO: each user avatar
     asset_pack_path('media/images/user.png')
   end
+
+  def bi_icon(icon_class)
+    content_tag 'span', '', class: "bi bi-#{icon_class}"
+  end
 end
