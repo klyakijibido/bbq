e79e576275f398486c5114709d86d5f456bc4858
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-08-17 18:35:26 +0300

    Expanded functionality current_user_can_edit?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user_can_edit?
 
-  def current_user_can_edit?(event)
-    user_signed_in? && event.user == current_user
+  def current_user_can_edit?(model)
+    user_signed_in? &&
+      (model.user == current_user || (model.try(:event).present? && model.event.user == current_user))
   end
 
   def configure_permitted_parameters
