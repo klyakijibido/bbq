45b41520b1edf3418567f06a6ba01d284bfb747c
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2023-08-18 19:41:04 +0300

    Add custom RegistrationsController, because device not permitted :name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   root 'events#index'
 
   resources :users, only: %i[show edit update]
